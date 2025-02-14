31b8eee71153738303aa29b4a0ae950423d22713
Andre Silva <andrenations@gmail.com>
2025-02-14 18:39:05 -0500

    add HomePage with ProductList component and sample data integration

--- sherupt/app/(root)/page.tsx
+++ sherupt/app/(root)/page.tsx
@@ -1,5 +1,13 @@
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '@/db/sample-data';
+
+
 const HomePage = () => {
-    return <>Latest Products</>;
-  };
-  
-  export default HomePage;
+  return (
+    <div className='space-y-8'>
+      {/* <h2 className='h2-bold'>Latest Products</h2> */}
+      <ProductList title="Save 25% on this items – now on sale!" data={sampleData.products} limit={4} />
+    </div>
+  );
+};
+export default HomePage;
