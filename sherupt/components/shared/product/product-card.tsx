9e80e7b4af6fad48880aa6aa76c75e015947cc79
Andre Silva <andrenations@gmail.com>
2025-02-14 18:41:02 -0500

    add ProductCard component with client-side rendering and conditional stock display
    - Integrated useIsClient hook to ensure styles and components render only after mounting on the client side.
    - Added dynamic product rendering with brand, name, image, and rating details.
    - Implemented conditional stock display to show either the product price or an "Out of Stock" message.

---
+++ sherupt/components/shared/product/product-card.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import ProductPrice from "./product-price";
+
+import { useIsClient } from "@/hooks/useIsClient";
+
+const ProductCard = ({ product }: { product: any }) => {
+  // Ensure styles are applied only after the component mounts
+
+  const mounted = useIsClient(); // Using the custom hook
+
+  if (!mounted) {
+    return null; // Return nothing until the component has mounted
+  }
+
+  return (
+    <Card className="w-full  max-w-sm">
+      <CardHeader className="p-0 items-center">
+        <Link href={`/product/${product.slug}`}>
+          <Image
+            priority={true}
+            src={product.images[0]}
+            alt={product.name}
+            className="aspect-square object-cover rounded"
+            height={300}
+            width={300}
+          />
+        </Link>
+      </CardHeader>
+      <CardContent className="p-4 grid gap-4">
+        <div className="text-xs">{product.brand}</div>
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="text-sm font-medium">{product.name}</h2>
+        </Link>
+        <div className="flex-between gap-4">
+          <p>{product.rating} stars</p>
+          {product.stock > 0 ? ( // ? then
+            <ProductPrice value={Number(product.price)} />
+          ) : (
+            // : else
+            <p className="text-destructive">Out of Stock</p>
+          )}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+export default ProductCard;
