0cbe17c09b8d21f4146d4518a3204220c3e4b73d
Andre Silva <andrenations@gmail.com>
2025-02-14 01:05:06 -0500

    add ThemeProvider from next-themes for theme management

--- sherupt/app/layout.tsx
+++ sherupt/app/layout.tsx
@@ -1,9 +1,10 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import '@/assets/styles/globals.css';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/assets/styles/globals.css";
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
+import { ThemeProvider } from "next-themes";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
@@ -12,8 +13,8 @@ export const metadata: Metadata = {
   },
 
   description: APP_DESCRIPTION,
-  
-  metadataBase: new URL(SERVER_URL)
+
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
@@ -22,8 +23,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
-      <body className={`${inter.className}`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableColorScheme
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
